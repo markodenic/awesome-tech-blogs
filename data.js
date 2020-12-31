19afb22b843c0d0817c7d94ef86cbcd42c377ee8
Ramya Chinnadurai <RamyaChinnadurai@users.noreply.github.com>
2021-01-01 01:11:34 +0530

    Update data.js

--- data.js
+++ data.js
@@ -130,5 +130,21 @@ module.exports = [
       'Next.js',
       'Node.js'
     ],
-  }
+  },
+  {
+    name: 'Ramya Chinnadurai (Rams)',
+    description: 'Full stack web developer. Passionate programmer and curious learner. Building http://100daysof.codes ',
+    url: 'http://rams.codes/',
+    twitter: '@code_rams',
+    tags: [
+      'HTML', 
+      'CSS',
+      'JavaScript',
+      'Web Development',
+      'React',
+      'Node.js',
+      'Chrome extensions',
+      'Next.js'
+    ],
+  },
 ];
