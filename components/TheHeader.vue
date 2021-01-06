66d5525b0bfbd6f3f79a50e3541b18a3fcc796b8
Marko D <marko.denic@web-ideenreich.at>
2021-01-06 16:52:04 +0100

    Logo style adjustment

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -3,24 +3,19 @@
     id="top"
     class="text-center pb-6"
   >
+    <img
+      src="/logo.svg"
+      width="70"
+      height="70"
+      alt="Awesome Tech Blogs"
+    />
+
     <h1>
-      <img src="/logo.svg" width="70" height="70" alt="Awesome Tech Blogs" />
       Tech blogs
     </h1>
+
     <p>
       This is a list of Awesome Tech Blogs.
     </p>
   </header>
 </template>
-
-<style scoped lang="less">
-h1 {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  margin-top: 0;
-
-  img { padding-bottom: 1rem; }
-}
-</style>
