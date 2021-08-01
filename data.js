ecb7c31dd8363a3ff25bbe870a6ff5d1e1fd2ecc
Karl <karl@kejk.tech>
2021-08-01 11:43:36 +0100

    Updated Karl Koch
    Patch to add additional frameworks

--- data.js
+++ data.js
@@ -29,7 +29,7 @@ module.exports = [
   },
   {
     name: 'Karl Koch',
-    description: 'Product Designer and Developer.',
+    description: 'Product Designer and UI Developer.',
     url: 'https://www.kejk.tech/thoughts',
     twitter: '@_kejk',
     tags: [
@@ -37,8 +37,10 @@ module.exports = [
       'CSS',
       'JavaScript',
       'Vue',
+      'Nuxt',
       'Swift',
       'SwiftUI',
+      'Figma API',
       'Figma plugins',
       'Product design'
     ],
