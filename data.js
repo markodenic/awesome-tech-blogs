78dde230a51d32f84488efd4c4b9082efa916f20
Node Code Formatter <formatter@1337z.ninja>
2021-09-26 10:24:33 +0000

    Formatted code

--- data.js
+++ data.js
@@ -86,12 +86,11 @@ module.exports = [
     tags: ["Networking", "Bluetooth", "Programming Languages"],
   },
   {
-  name: "Marko ⚡ Denic",
+    name: "Marko ⚡ Denic",
     description: "Web developer. IT consultant and trainer.",
     url: "https://markodenic.com/blog/",
     twitter: "@denicmarko",
     tags: [
-
       "HTML",
       "CSS",
       "JavaScript",
