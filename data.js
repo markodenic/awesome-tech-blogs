0448075f75155bb5b7679368dd26562ae0676e93
Aymane Mimouni <aymanemx@gmail.com>
2022-02-01 15:06:51 +0100

    fixes :)

--- data.js
+++ data.js
@@ -2603,8 +2603,8 @@ module.exports = [
   {
     name: "Aymane Mimouni",
     description:
-      "I am Aymane MIMOUNI a Software Engineer and a passionate Python developer from Morocco who loves to build and deliver quality products.",
-    url: "https://aymanemx.com",
+      "Software Engineer and a passionate Python developer from Morocco who loves to build and deliver quality products.",
+    url: "https://aymanemx.com/posts",
     twitter: "https://twitter.com/aymane_max",
     tags: [
       "Python",
