b0c33fa653b51c07b2d6fa4a45c9709d83f7e46c
Madza <madars.biss@gmail.com>
2022-02-02 13:08:44 +0200

    Added Madza's blog

--- data.js
+++ data.js
@@ -2600,4 +2600,23 @@ module.exports = [
       "SSIS",
     ],
   },
+  {
+    name: 'Madza',
+    description: 'I build custom WEB solutions and write about DEV',
+    url: 'https://madza.dev/blog',
+    twitter: '@madzadev',
+    tags: [
+      "HTML",
+      "CSS",
+      "TypeScript",
+      "React",
+      "Front End",
+      "GitHub",
+      "Web Development",
+      "Career",
+      "Jamstack",
+      "Svelte",
+      "Next.js",
+    ]
+}
 ];
