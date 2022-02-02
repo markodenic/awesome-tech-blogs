89fe32131e0d98228a3d5cc2d5c04ec44ef54f6b
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2022-02-02 08:19:30 +0100

    Put blog in random place.

--- data.js
+++ data.js
@@ -2327,6 +2327,20 @@ module.exports = [
     ],
   },
   {
+    name: "Alexandre Nédélec",
+    description: "Software developer living in Bordeaux, France ⋅ Working mainly in C# / .NET on Azure ⋅ Love discovering new tools and technologies",
+    url: "https://www.techwatching.dev/",
+    twitter: "@TechWatching",
+    tags: [
+      "C#",
+      ".NET"",
+      "Azure",
+      "Cloud",
+      "Software Development",
+      "DevOps"
+    ],
+  },
+  {
     name: "Tapas Adhikary(GreenRoots Blog)",
     description:
       "Articles about Tech, JavaScript, Web Technologies, and Career development. ",
@@ -2598,20 +2612,6 @@ module.exports = [
       "Azure",
       "ETL",
       "SSIS",
-    ],
-  },
-    {
-    name: "Alexandre Nédélec",
-    description: "Software developer living in Bordeaux, France ⋅ Working mainly in C# / .NET on Azure ⋅ Love discovering new tools and technologies",
-    url: "https://www.techwatching.dev/",
-    twitter: "@TechWatching",
-    tags: [
-      "C#",
-      ".NET"",
-      "Azure",
-      "Cloud",
-      "Software Development",
-      "DevOps"
     ],
   },
 ];
