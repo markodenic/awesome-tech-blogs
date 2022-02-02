43d68aa9d5e65a2fddb7e52e863948a3bb0743eb
Alexandre Nédélec <15186176+TechWatching@users.noreply.github.com>
2022-02-02 08:16:04 +0100

    Add Alexandre Nédélec's blog.

--- data.js
+++ data.js
@@ -2600,4 +2600,18 @@ module.exports = [
       "SSIS",
     ],
   },
+    {
+    name: "Alexandre Nédélec",
+    description: "Software developer living in Bordeaux, France ⋅ Working mainly in C# / .NET on Azure ⋅ Love discovering new tools and technologies",
+    url: "https://www.techwatching.dev/",
+    twitter: "@TechWatching",
+    tags: [
+      "C#",
+      ".NET"",
+      "Azure",
+      "Cloud",
+      "Software Development",
+      "DevOps"
+    ],
+  },
 ];
