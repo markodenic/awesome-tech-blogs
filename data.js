577bce2676902dc4ac27cb61945b20360867f852
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2022-02-07 13:55:35 +0000

    Add pawelgrzybek.com

--- data.js
+++ data.js
@@ -2671,4 +2671,11 @@ module.exports = [
       "Shopyo",
     ],
   },
+  {
+    name: 'Pawel Grzybek',
+    description: "I'm Pawel Grzybek, a Software Engineer from Poland. I build Web stuff and write about it. A keen cyclist and a funky/jazz records collector.",
+    url: 'https://pawelgrzybek.com',
+    twitter: '@pawelgrzybek',
+    tags: ['HTML', 'CSS', 'JavaScript', 'Web Development', 'Front End', 'Node.js'],
+}
 ];
