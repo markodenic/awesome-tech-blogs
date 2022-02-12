082bbb1dfdea7e88cbb51a4e2fbdaf6168afe508
Node Code Formatter <formatter@1337z.ninja>
2022-02-12 19:07:00 +0000

    Formatted code

--- data.js
+++ data.js
@@ -2946,17 +2946,11 @@ module.exports = [
   },
   {
     name: "iRead Blog",
-    description:"iRead is a multi-user blogging platform where you'll find technical articles and project tutorials related to Python, Javascript, Flutter and much more ",
+    description:
+      "iRead is a multi-user blogging platform where you'll find technical articles and project tutorials related to Python, Javascript, Flutter and much more ",
     url: "https://ireadblog.com",
     twitter: "@iReadBot",
-    tags: [
-      "Python",
-      "Java",
-      "DSA",
-      "Javascript",
-      "Flutter",
-      "Web Development",
-    ],
+    tags: ["Python", "Java", "DSA", "Javascript", "Flutter", "Web Development"],
   },
   {
     name: "Helmer Davila",
