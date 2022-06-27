3460abf5c32353aa68d8598a4dcb538b27dc2c08
Vipul <vipulsoniab17@gmail.com>
2022-06-27 20:24:13 +0530

    added blogs details

--- data.js
+++ data.js
@@ -3887,5 +3887,22 @@ module.exports = [
       "Web Development",
       "Beginner",
     ],
+  },
+  {
+    name: "Vipul Soni",
+    description: "Full Stack Web Developer.Write blogs about Web Developement",
+    url: "https://medium.com/@vipulsoniab17_40156",
+    twitter: "https://twitter.com/Vipulso06279659",
+    tags: [
+      "React",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Web Development",
+      "MongoDB",
+      "Express",
+      "Node",
+      "TailwindCSS"
+    ],
   }
 ];
