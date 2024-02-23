affd8ab7fdeaa853f23ac087d30617c630b4b542
Anthony <ajxs@panoptic.online>
2024-02-23 16:47:27 +1100

    Move ajxs.me entry to random position

--- data.js
+++ data.js
@@ -4570,6 +4570,18 @@ module.exports = [
     ],
   },
   {
+    name: 'Ajxs',
+    description: 'Reverse-engineering vintage synthesisers, programming, and the small-web',
+    url: 'https://ajxs.me',
+    tags: [
+      'Assembly',
+      'Low-level',
+      '8-bit',
+      'Reverse-engineering',
+      'Synthesizers',
+    ],
+  },
+  {
     name: 'Mahdi',
     description: 'Senior Software Engineer and Bootstrapper',
     url: 'https://mahdix.com',
@@ -4624,16 +4636,4 @@ module.exports = [
     twitter: "@julian_rubisch",
     tags: ['Ruby', 'JavaScript', 'Code Quality', 'Tech Debt', 'Refactoring', 'Code Health', 'Code Smells']
   },
-  {
-    name: 'Ajxs',
-    description: 'Reverse-engineering vintage synthesisers, programming, and the small-web',
-    url: 'https://ajxs.me',
-    tags: [
-      'Assembly',
-      'Low-level',
-      '8-bit',
-      'Reverse-engineering',
-      'Synthesizers',
-    ],
-  }
 ];
