707555ad3e5b448f907f65f5c19fff356a18204f
jellydisc <jellydiscdev@gmail.com>
2025-04-29 16:13:42 -0500

    Add Christian Tech Jobs

--- data.js
+++ data.js
@@ -4510,4 +4510,16 @@ module.exports = [
       "FinOps"
     ],
   },
+  {
+    name: "Christian Tech Jobs",
+    description: "Articles about Christian tech trends and career advice for the Christian tech space.",
+    url: "https://www.christiantechjobs.io/blog",
+    twitter: "@ctj_io",
+    tags: [
+      "Career",
+      "Self-Development",
+      "Tech",
+      "Jobs"
+    ],
+  },
 ];
