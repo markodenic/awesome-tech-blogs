640319b18eeeda011b5e3d757eb657c90b9a0407
Marko Denic <marko.denic84@gmail.com>
2025-12-17 08:43:17 +0100

    Add fallback: true to force 404.html instead 200.html file to be generated

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target (https://go.nuxtjs.dev/config-target)
   target: 'static',
 
+  generate: {
+    fallback: true
+  },
+
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
     htmlAttrs: {
