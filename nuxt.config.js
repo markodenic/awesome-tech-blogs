99038c3ab00defcfbf32ee7e3a72d9d5dd3b661b
Marko Denic <marko.denic84@gmail.com>
2026-01-12 20:13:08 +0100

    Fix deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  ssr: false,
+  nitro: {
+    preset: 'static'
+  },
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
 
