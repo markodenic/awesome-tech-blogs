f14aa8c3eb16d4c6fc1e24fadf81737378aa5269
Marko Denic <marko.denic84@gmail.com>
2026-01-14 18:27:46 +0100

    Add category title

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,12 +1,25 @@
 <template>
   <header id="top" class="text-center pb-6">
-    <h1>Tech blogs</h1>
+    <h1>{{ title }}</h1>
     <p>Find amazing tech-related articles by the Tech community!</p>
   </header>
 </template>
 
 <script>
 export default {
-  name: 'TheHeader'
+  name: 'TheHeader',
+  computed: {
+    title() {
+      const category = this.$route.params.category
+      if (category) {
+        const formattedCategory = category
+          .split('-')
+          .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(' ')
+        return `${formattedCategory} - Tech blogs`
+      }
+      return 'Tech blogs'
+    }
+  }
 }
 </script>
